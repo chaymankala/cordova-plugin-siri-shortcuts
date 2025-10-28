f200f766b05f4c36958f98e6bdff827a98ac307a
sowmya-kiot <sowmya@kiot.io>
2025-10-28 14:15:02 +0530

    [fix]- app delegate issue fix for siri shortcuts

--- src/ios/AppDelegate+SiriShortcuts.m
+++ src/ios/AppDelegate+SiriShortcuts.m
@@ -16,14 +16,29 @@ static void * UserActivityPropertyKey = &UserActivityPropertyKey;
 - (BOOL)application:(UIApplication *)application
 continueUserActivity:(NSUserActivity *)userActivity
  restorationHandler:(void (^)(NSArray *))restorationHandler {
+    
     NSString *bundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
     if ([userActivity.activityType isEqualToString:[NSString stringWithFormat:@"%@.shortcut", bundleIdentifier]])
     {
+        NSLog(@"Setting userActivity on appDelegate");
         self.userActivity = userActivity;
-
         return YES;
     }
-
+    
+    NSLog(@"Activity type didn't match - checking if Truecaller can handle it");
+    
+    // Forward to Truecaller SDK if it exists
+    Class tcClass = NSClassFromString(@"TCTrueSDK");
+    if (tcClass && [tcClass respondsToSelector:@selector(sharedManager)]) {
+        id tcManager = [tcClass performSelector:@selector(sharedManager)];
+        if ([tcManager respondsToSelector:@selector(application:continueUserActivity:restorationHandler:)]) {
+            NSLog(@"Forwarding to Truecaller SDK");
+            return [tcManager application:application
+                      continueUserActivity:userActivity
+                        restorationHandler:restorationHandler];
+        }
+    }
+    
     return NO;
 }
 
